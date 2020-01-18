2bdeb14dcd6324de2bbe8f717a94d674852de3ec
Sweetie Piggy Apps <sweetiepiggyapps@gmail.com>
2020-01-18 17:47:41 -0600

    hide _id in shapes

--- app/controllers/shapes_controller.rb
+++ app/controllers/shapes_controller.rb
@@ -1,11 +1,11 @@
 class ShapesController < ApplicationController
   def index
     @shapes = Shape.all
-    render json: @shapes.to_json
+    render json: @shapes.to_json(:except => :_id)
   end
 
   def show
     @shape = Shape.where(shape_id: params[:id])
-    render json: @shape.to_json
+    render json: @shape.to_json(:except => :_id)
   end
 end
